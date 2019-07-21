416a64569534d39af81a828d78105720686a3281
PsykeSPB <PsykeSPB@gmail.com>
2019-07-21 16:15:09 +0300

    testing woocommerce hooks

--- wp-mts-fiscalization.php
+++ wp-mts-fiscalization.php
@@ -54,12 +54,10 @@ function test_before_mts_postback() {
 	);
 	$response = wp_remote_post( $url, $args );
 
-	// if( is_wp_error( $response ) ) {
-	// 	$error_message = $response->get_error_message();
-	// 	echo "Something gone wrong: $error_message";
-	// } else {
-	// 	echo "postback send: $response";
-	// }
-
-	echo $response;
+	if( is_wp_error( $response ) ) {
+		$error_message = $response->get_error_message();
+		echo "Something gone wrong: $error_message";
+	} else {
+		echo "postback send: $response";
+	}
 }
