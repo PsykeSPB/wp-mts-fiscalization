9cff1097fe90b6915d96b152f4552a5f76ce1c63
PsykeSPB <PsykeSPB@gmail.com>
2019-07-27 19:21:36 +0300

    fixed message body

--- wp-mts-fiscalization.php
+++ wp-mts-fiscalization.php
@@ -97,9 +97,8 @@ if(!class_exists('MTSFiscalization')) {
 
 		public static function send_postback($order_id) {
 			$order = wc_get_order($order_id);
-			$body = json_encode(MTSFiscalization::getPackagedOrder($order), JSON_UNESCAPED_UNICODE);
+			$body = json_encode(MTSFiscalization::getPackagedOrder($order), JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
 			$body = preg_replace('/"(\d+)\.(\d{2})"/', '$1.$2', $body);
-			$body = preg_replace('/:\\\/\\\//', '://', $body);
 
 			echo 'Request:';
 			echo '<pre>';
