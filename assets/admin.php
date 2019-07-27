aa9698e86ffadd648f52587460cc52d06fb2c039
PsykeSPB <PsykeSPB@gmail.com>
2019-07-27 14:31:11 +0300

    added settings to settings page

--- assets/admin.php
+++ assets/admin.php
@@ -3,7 +3,7 @@
 	<form method="post" action="options.php">
 		<?php settings_fields('mts_fiscalization_options_organization'); ?>
 		<h3>Данные организации:</h3>
-		<p>Заполняется в соответствии с данными из личном кабинета МТС.</p>
+		<p>Заполняются в соответствии с данными из личном кабинета МТС.</p>
 		<table>
 			<tr valign="top">
 				<th scope="row">
@@ -11,6 +11,7 @@
 				</th>
 				<td>
 					<input type="email"
+						id="mts_fiscalization_organization_email"
 						name="mts_fiscalization_organization_email"
 						value="<?php echo get_option('mts_fiscalization_organization_email'); ?>"
 					/>
